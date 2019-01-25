a23f2831a88cd8c564538e8339d491b616dddc6e
Louis Dionne <ldionne@apple.com>
2019-01-25 16:33:31 -0500

    Fix issue with back references in regular expression

---
+++ libcxx/test/std/re/re.alg/re.alg.search/back_reference.pass.cpp
@@ -0,0 +1,28 @@
+//===----------------------------------------------------------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+// <regex>
+
+// template <class BidirectionalIterator, class Allocator, class charT, class traits>
+//     bool
+//     regex_search(BidirectionalIterator first, BidirectionalIterator last,
+//                  match_results<BidirectionalIterator, Allocator>& m,
+//                  const basic_regex<charT, traits>& e,
+//                  regex_constants::match_flag_type flags = regex_constants::match_default);
+
+#include <regex>
+#include <cassert>
+
+
+// PRXXXXX
+int main() {
+  std::regex re("(a)xxx(?=\\1)");
+  char const* str = "axxxa";
+  std::cmatch matches;
+  assert(std::regex_search(str, matches, re));
+}
