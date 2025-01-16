9e19c80567ca7c7d77fffef8da27a171b92011bd
Louis Dionne <ldionne.2@gmail.com>
2025-01-16 13:30:26 -0500

    Simplify scripts'

--- benchmark-with-libcxx.sh
+++ benchmark-with-libcxx.sh
@@ -7,38 +7,23 @@ function git-repo-root() {
 }
 
 UMBRELLA_ROOT="$(git-repo-root)"
+BUILD_DIR="${UMBRELLA_ROOT}/build/libcxx"
 
 export CXX=clang++
 export CC=clang
 ${CXX} --version
 
-# ${UMBRELLA_ROOT}/install/${version} must already contain pre-built content for older releases
-version=${1}
-
-BUILD_DIR="${UMBRELLA_ROOT}/build/${version}"
-INSTALL_DIR="${UMBRELLA_ROOT}/install/${version}"
-rm -rf "${BUILD_DIR}" "${INSTALL_DIR}"
-mkdir -p "${BUILD_DIR}" "${INSTALL_DIR}"
+rm -rf "${BUILD_DIR}"
+mkdir -p "${BUILD_DIR}"
 
 # Prepare the test suite
 cmake -S "${UMBRELLA_ROOT}/runtimes" -B "${BUILD_DIR}" -GNinja -DCMAKE_BUILD_TYPE=RelWithDebInfo    \
-        -DCMAKE_INSTALL_PREFIX="${INSTALL_DIR}"                                                     \
         -DLLVM_ENABLE_RUNTIMES="libcxx;libcxxabi"                                                   \
         -DLIBCXXABI_USE_LLVM_UNWINDER=OFF
-
-ninja -C "${BUILD_DIR}" install-cxx install-cxxabi
 ninja -C "${BUILD_DIR}" cxx-test-depends
 
 # Run the benchmarks
 ./libcxx/utils/libcxx-lit "${BUILD_DIR}"                            \
     --show-all -j1 --time-tests                                     \
-    --param "prefix=${UMBRELLA_ROOT}/install/${version}"            \
     --param optimization=speed                                      \
-    libcxx/test/benchmarks/{containers,algorithms}
-
-# Aggregate all the benchmark results into a common location
-mkdir -p ${BUILD_DIR}/benchmark-results
-for file in $(find "${BUILD_DIR}/libcxx/test/benchmarks" -name 'benchmark-result.json'); do
-    bench=$(echo $file | grep --only-matching -E 'Output/(.+).bench.cpp' | sed 's_Output/__' | sed 's/.bench.cpp//')
-    cp ${file} ${BUILD_DIR}/benchmark-results/${bench}.json
-done
+    libcxx/test/benchmarks/containers/{vector,deque}.bench.cpp
--- benchmark-with-libstdcxx.sh
+++ benchmark-with-libstdcxx.sh
@@ -8,35 +8,26 @@ function git-repo-root() {
 
 UMBRELLA_ROOT="$(git-repo-root)"
 BUILD_DIR="${UMBRELLA_ROOT}/build/libstdcxx"
-INSTALL_DIR="${UMBRELLA_ROOT}/install/libstdcxx"
 
 export CXX=clang++
 export CC=clang
 ${CXX} --version
 
-rm -rf "${BUILD_DIR}" "${INSTALL_DIR}"
-mkdir -p "${BUILD_DIR}" "${INSTALL_DIR}"
-args=(${@})
+rm -rf "${BUILD_DIR}"
+mkdir -p "${BUILD_DIR}"
 
+# Prepare the test suite
 cmake -S "${UMBRELLA_ROOT}/runtimes" -B "${BUILD_DIR}" -GNinja -DCMAKE_BUILD_TYPE=RelWithDebInfo    \
-        -DCMAKE_INSTALL_PREFIX="${INSTALL_DIR}"                                                     \
         -DLLVM_ENABLE_RUNTIMES="libcxx;libcxxabi;libunwind"                                         \
         -DBENCHMARK_USE_LIBSTDCXX=ON                                                                \
-        -DLIBCXX_TEST_CONFIG=stdlib-libstdc++.cfg.in                                                \
-        ${args[@]}
+        -DLIBCXX_TEST_CONFIG=stdlib-libstdc++.cfg.in
 
 ninja -C "${BUILD_DIR}" cxx-test-depends
 
+# Run the benchmarks
 ./libcxx/utils/libcxx-lit "${BUILD_DIR}" --show-all -j1 --time-tests                                        \
                                          --param optimization=speed                                         \
                                          --param libstdcxx-install-prefix=/opt/homebrew/Cellar/gcc/14.1.0_1 \
                                          --param libstdcxx-version=14                                       \
                                          --param libstdcxx-triple=aarch64-apple-darwin23                    \
-                                         libcxx/test/benchmarks/{containers,algorithms}
-
-# Aggregate all the benchmark results into a common location
-mkdir -p ${BUILD_DIR}/benchmark-results
-for file in $(find "${BUILD_DIR}/libcxx/test/benchmarks" -name 'benchmark-result.json'); do
-    bench=$(echo $file | grep --only-matching -E 'Output/(.+).bench.cpp' | sed 's_Output/__' | sed 's/.bench.cpp//')
-    cp ${file} ${BUILD_DIR}/benchmark-results/${bench}.json
-done
+                                         libcxx/test/benchmarks/containers/{vector,deque}.bench.cpp
